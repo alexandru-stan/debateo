bd399cee7d14383ef7765f779c214efe70b6e2db
Alexandru Stan <alexxst56@gmail.com>
2025-01-05 17:58:29 +0100

    LATERAL MENU VISUAL GLITCH FIXED

--- app/dist/index.js
+++ app/dist/index.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51875" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50946" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
